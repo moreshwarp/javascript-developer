df3efcb0a45dedcefc1ec32bf2f9d5be2ee738cf
Moreshwar Pidadi <44057383+moreshwarp@users.noreply.github.com>
2022-12-11 22:19:53 +0530

    JavaScrpit Data types in JavaScript

--- Javascript/values.js
+++ Javascript/values.js
@@ -1,4 +1,14 @@
 console.log("Values in JavaScript");
 
 // Here in JavaScript, number is an datatype,
-// And beloew are the references for the same
+// And below are the references for the same
+// There are total
+
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 5. Null
+// 7. Symbol
+// 8. Object
