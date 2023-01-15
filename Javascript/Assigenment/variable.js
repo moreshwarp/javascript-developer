b29e27b4a5f68da7a79989f72311d891c2a9c63e
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-01-16 00:20:07 +0530

    Javascript Assignmet: String Splits()

--- Javascript/Assigenment/variable.js
+++ Javascript/Assigenment/variable.js
@@ -1,3 +1,5 @@
+// Question 1
+
 // String DataType
 const firstName = "Moreshwar";
 console.log("Value is " + firstName + " Datatype " + typeof firstName);
@@ -21,3 +23,38 @@ console.log(nullDataType === null);
 // BigInt DataType
 const bigIntData = 16464646764694674764964747614n;
 console.log("Value is " + bigIntData + " Datatype " + typeof bigIntData);
+
+// Question 2
+
+const selfProfile = {
+  firstName: "Moreshwar",
+  lastName: "Pidadi",
+  matrialStatus: false,
+  country: "India",
+  age: 26,
+};
+
+console.log(`Hi There,
+            My name is ${selfProfile.firstName} ${selfProfile.lastName} I am from ${selfProfile.country} and ${selfProfile.age} and current marritial status is ${selfProfile.matrialStatus}`);
+
+// Question 3
+const userName = "Moreshwar";
+console.log(userName);
+console.log(userName.toUpperCase());
+
+// Question 4
+
+const wordString = "Hello World Demo";
+console.log(wordString.trim().toLowerCase().includes("world"));
+
+// Question 5
+const splitString = "Hello This Is An Split String Ex.";
+console.log(splitString.split(" "));
+const arrayString = splitString.split(" ");
+console.log(arrayString[0]);
+console.log(arrayString[1]);
+console.log(arrayString[3]);
+console.log(arrayString[4]);
+console.log(arrayString[5]);
+console.log(arrayString[2]);
+console.log(arrayString[4]);
