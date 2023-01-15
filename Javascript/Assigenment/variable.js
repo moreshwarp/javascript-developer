f5624e51918fc93ae34b3850bb62c35bad0d1804
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-01-15 20:51:58 +0530

    Javascript Assignmet: Variables

--- Javascript/Assigenment/variable.js
+++ Javascript/Assigenment/variable.js
@@ -1,19 +1,23 @@
 // String DataType
 const firstName = "Moreshwar";
-console.log("Variable is " + firstName + " Datatype " + typeof firstName);
+console.log("Value is " + firstName + " Datatype " + typeof firstName);
 
 // Boolean DataType
 const varBoolean = true;
-console.log(varBoolean + " Datatype " + typeof varBoolean);
+console.log("Value is " + varBoolean + " Datatype " + typeof varBoolean);
 
 // Undefined DataType
 const testUndefined = undefined;
-console.log(testUndefined + " Datatype " + typeof testUndefined);
+console.log("Value is " + testUndefined + " Datatype " + typeof testUndefined);
 
 // Null DataType
 const nullDataType = null;
 // const nullDataType = !null;
-console.log(nullDataType + " Datatype " + typeof nullDataType);
+console.log("Value is " + nullDataType + " Datatype " + typeof nullDataType);
 // Here the typeOf will give an data type of an onject as in JS you will then have to use === operator to see if it's Null or not.
 console.log("The True value says that the dataType is Null");
 console.log(nullDataType === null);
+
+// BigInt DataType
+const bigIntData = 16464646764694674764964747614n;
+console.log("Value is " + bigIntData + " Datatype " + typeof bigIntData);
