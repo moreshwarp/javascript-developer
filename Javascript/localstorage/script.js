c586bf183b8d80676e8d4ac88af7d59f9d1b845c
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-03 00:02:45 +0530

    LocalStorage 3

--- Javascript/localstorage/script.js
+++ Javascript/localstorage/script.js
@@ -3,3 +3,10 @@ let value = prompt("Enter value");
 
 localStorage.setItem(key, value);
 console.log(` the Key is ${key} and its value is ${localStorage.getItem(key)}`);
+
+key = prompt("Enter Key for which you want to remove the item");
+localStorage.getItem(key);
+console.log(
+  `Item with ${key} Name and it has value ${localStorage.getItem(key)}`
+);
+localStorage.removeItem(key);
