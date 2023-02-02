0873df4a7fe397795de732adfc3d2ee244b72b67
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-03 01:48:05 +0530

    LocalStorage 4

--- Javascript/localstorage/script.js
+++ Javascript/localstorage/script.js
@@ -10,3 +10,20 @@ console.log(
   `Item with ${key} Name and it has value ${localStorage.getItem(key)}`
 );
 localStorage.removeItem(key);
+
+// localStorage.clear();
+// To calculate the length of local storage
+console.log(localStorage.length);
+console.log(localStorage.key(2));
+console.log(localStorage.getItem(2));
+
+let aObject = {
+  fname: "Moreshwar",
+  lname: "Pidadi",
+  empId: 19274,
+  nickName: "More",
+};
+
+let value_1 = JSON.stringify(aObject);
+localStorage.setItem("object", value_1);
+console.log(localStorage.getItem(JSON.parse(value_1)));
