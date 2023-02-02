cb560db23d2384d153a787f2ac9b9bdd4be79cf6
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-02 23:39:56 +0530

    LocalStorage

---
+++ Javascript/localstorage/script.js
@@ -0,0 +1,7 @@
+localStorage.setItem("fName", "Moreshwar");
+localStorage.setItem("lName", "Pidadi");
+localStorage.setItem("empId", "19247");
+
+console.log(localStorage.getItem("fName"));
+console.log(localStorage.getItem("lName"));
+console.log(localStorage.getItem("empId"));
