147bc8589e705af14f0efe2c6851e4fe3b153016
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-02 23:54:42 +0530

    LocalStorage 2

--- Javascript/localstorage/script.js
+++ Javascript/localstorage/script.js
@@ -1,7 +1,5 @@
-localStorage.setItem("fName", "Moreshwar");
-localStorage.setItem("lName", "Pidadi");
-localStorage.setItem("empId", "19247");
+let key = prompt("Enter Key");
+let value = prompt("Enter value");
 
-console.log(localStorage.getItem("fName"));
-console.log(localStorage.getItem("lName"));
-console.log(localStorage.getItem("empId"));
+localStorage.setItem(key, value);
+console.log(` the Key is ${key} and its value is ${localStorage.getItem(key)}`);
