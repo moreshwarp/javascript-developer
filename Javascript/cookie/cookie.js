9d9798fc8d80c53f446e1ad61904bdeb96b9c810
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-06 02:29:51 +0530

    Cookie: encodeURLComponent, Session Cookie, decodeURIComponent

--- Javascript/cookie/cookie.js
+++ Javascript/cookie/cookie.js
@@ -8,11 +8,22 @@ document.cookie = "name4=Moreshwar4";
 
 console.log(document.cookie);
 
+// document.cookie = `{key}` + "=" + `{value}`;
+// This won't set Key Value pair properly in case of special character's
+
 // Encode URI Component That Helps to keep the valid formating. It is used like this
 
 let key = prompt("Enter the Name");
 let value = prompt("Enter Value");
-
 document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(value);
+console.log(document.cookie);
+console.log(decodeURIComponent("%40%23%40%23%40!%23!%40"));
+
+// Cookie Options
+
+document.cookie =
+  "user=Moreshwar; path=/; expires=Tue, 6 Feb 2023 02:06:00 GMT";
+
+document.cookie = "user10=Moreshwar; path=/; max-age=1000";
 
 console.log(document.cookie);
