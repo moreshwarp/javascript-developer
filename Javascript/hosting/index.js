ffc448599e5129fc4652ca987c7d83768e391c4e
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-25 23:50:49 +0530

    Hosting part : 1

--- Javascript/hosting/index.js
+++ Javascript/hosting/index.js
@@ -1,10 +1,22 @@
-console.log(test);
-getName(test);
+// Example with var
 
-var test = "Moreshwar";
+console.log(x);
+// Hosted the decleration but not Initilized hence the memory allocation will take place and value initially stored in it will/ shall be undefined.
 
-function getName(test) {
-  console.log(test);
-}
+getName(); // Hosted and Initilized will give the intended o/p i.e what is written in Hosting in JavaScript
+// Here the function code is been stored in the allocated memeory and hence we will have an expected output.
+// Even After giving the call to the function
 
-getName(test);
+var x = 10;
+// function getName() {
+//   console.log("Hosting in JavaScript");
+// }
+
+// Arrow Function
+
+var getName = () => {
+  console.log("Hosting in JavaScript");
+};
+
+// Here in Arrow function will behave like a variable and hence it will give an err getName is not a function.
+// getName();
