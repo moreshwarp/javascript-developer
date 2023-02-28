5559d8f63372d2457c0aef523da4eaf25192d182
moreshwarp <44057383+moreshwarp@users.noreply.github.com>
2023-02-28 23:48:49 +0530

    GSPAAN Assignment

--- Javascript/hosting/index.js
+++ Javascript/hosting/index.js
@@ -1,22 +1,34 @@
 // Example with var
 
-console.log(x);
+//console.log(x);
 // Hosted the decleration but not Initilized hence the memory allocation will take place and value initially stored in it will/ shall be undefined.
 
-getName(); // Hosted and Initilized will give the intended o/p i.e what is written in Hosting in JavaScript
+//getName(); // Hosted and Initilized will give the intended o/p i.e what is written in Hosting in JavaScript
 // Here the function code is been stored in the allocated memeory and hence we will have an expected output.
 // Even After giving the call to the function
 
-var x = 10;
+// var x = 10;
 // function getName() {
 //   console.log("Hosting in JavaScript");
 // }
 
 // Arrow Function
 
-var getName = () => {
-  console.log("Hosting in JavaScript");
-};
+// var getName = () => {
+//   console.log("Hosting in JavaScript");
+// };
 
 // Here in Arrow function will behave like a variable and hence it will give an err getName is not a function.
 // getName();
+
+console.log(getName()); // Function will give o/p: Hosting in  JavaScript.
+
+ console.log(x); // Referernce Error: Cannot access the variable before its initilized.
+
+let x = 10; // Initilization.
+
+function getName() {
+  console.log("Hosting in  JavaScript");
+}
+
+// console.log(localStorage.getItem("value"));
